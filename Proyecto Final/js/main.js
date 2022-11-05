cf4b4762d990d2d952efe7dad8208b6b09a43848
Francomull <francomuller73@gmail.com>
2022-11-05 13:42:20 -0300

    terminado con array

--- Proyecto Final/js/main.js
+++ Proyecto Final/js/main.js
@@ -1,34 +1,8 @@
-const btn1 = document.querySelector('.btn1'),
-btn2 = document.querySelector('.btn2'),
-btn3 = document.querySelector('.btn3'),
-btn4 = document.querySelector('.btn4')
 
+const masterDiv1 = document.getElementById("masterDiv")
+const verCarrito = document.getElementById("verCarrito")
+const divCarrito = document.getElementById("divCarrito")
 
-const productos =[
-    {class: cards, 
-    nombre: "Remera Adidas Blanca", 
-    precio: 5999,
-    img:  "../images/",
-    },
-
-    {class: cards2,
-    nombre: "Rompevientos Jordan", 
-    precio: 8899,
-    img: "../images/rompeJordan.jpg"
-    },
-    
-    {class: cards3,
-    nombre: "Rompevientos Nike",
-    precio: 9599,
-    img: "images/rompeNike.JPG"
-    },
-
-    {class: cards4,
-    nombre: "Short Adidas", 
-    precio: 4799,
-    img:"images/shortAdi.JPG"
-    }
-];
 
 
 let carrito = [];
@@ -37,35 +11,76 @@ let carrito = [];
 
 productos.forEach((product) =>{
     let content = document.createElement("div");
+    content.className = "productos"
     content.innerHTML = `
     <img src="${product.img}">
     <h3>${product.nombre} </h3>
     <p>${product.precio} $</p>
     `
 
-    shopContent.append(content);
+    masterDiv1.append(content);
 
-    let comprar = document.createElement ("buttom");
+    let comprar = document.createElement ("button");
     comprar.innerText = "Añadir al Carrito";
+    comprar.className = "btnComprar"
 
     content.append(comprar);
+    comprar.addEventListener("click", ()=>{
+        carrito.push({
+            id: product.class,
+            img: product.img,
+            nombre: product.nombre,
+            precio: product.precio
+        })
+    })
+})
+
+
+verCarrito.addEventListener("click", ()=>{
+    divCarrito.innerHTML = "";
+    divCarrito.style.display = "grid"
+    const carritoContent = document.createElement("div");
+    carritoContent.className = "carritoTodo"
+    carritoContent.innerHTML = `
+    <h1> ¿Listo para finalizar tu compra? </h1>
+    `
+    divCarrito.append(carritoContent);
+
+    const carritoButton = document.createElement("button")
+    carritoButton.innerText = "Cerrar"
+
+    carritoButton.className = "btnCarrito"
+
+    carritoButton.addEventListener('click', ()=>{
+        divCarrito.style.display = "none"
+    })
+
+    carritoContent.append(carritoButton);
+
+    carrito.forEach((product) =>{
+
+        let carritoTodo = document.createElement("div")
+        carritoTodo.calssName = "carritoTodo"
+        carritoTodo.innerHTML = `
+        <img src= "${product.img}">
+        <h3> "${product.nombre} "</h3>
+        <p> "${product.precio} " </p>
+        `;
+
+        divCarrito.append(carritoTodo)
+    })
+
+
+    const total = carrito.reduce ((acc, el)=> acc + el.precio, 0)
+
+    const total2 = document.createElement("div")
+    total2.calssName = "totalFooter"
+    total2.innerHTML = `Este es su total a pagar: $ ${total}`
+
+    divCarrito.append(total2)
 })
 
 
 
 
-
-
-
-
-
-btn1.addEventListener("click", ()=>{
-    carrito.push({
-        id: product.class,
-        img: product.img,
-        nombre: product.nombre,
-        precio: product.precio
-    });
-    console.log(carrito)
-} )
 
